451cd0977b4d5e0d34cb30fc16d3e4bd608e31ef
Thinhnthe172159 <101821797+Thinhnthe172159@users.noreply.github.com>
2025-05-15 22:39:32 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+
+// () => {}
+
+const App = ()=> {
   const [count, setCount] = useState(0)
 
+
+  // function MyFunction(){
+  //   console.log("Ahihi")
+  // }
+
+  const MyFunction = ()=>{
+    console.log("hello thinh");
+  }
+   MyFunction();
+  
   return (
     <>
       <div>
